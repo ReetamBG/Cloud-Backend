4a87329789fec13256c233033c1fd8ad74bb6b48
ReetamBG <r33t4m@gmail.com>
2024-12-28 17:22:00 +0530

    added cors

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ DB = DBHelper()
 
 app.add_middleware(
 	CORSMiddleware,
-	allow_origins=["http://localhost"],
+	allow_origins=[""],
+	allow_origin_regex=r"https?://localhost:.*",
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
