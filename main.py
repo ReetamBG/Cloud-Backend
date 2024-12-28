51190e071d9b383bdb9328affa74a025281101fe
ReetamBG <r33t4m@gmail.com>
2024-12-28 16:03:51 +0530

    switched back db

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ DB = DBHelper()
 
 app.add_middleware(
 	CORSMiddleware,
-	allow_origins=["*"],
+	allow_origins=["http://localhost"],
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
