673b24fbf6d2c2e459f78e52ecf832721203d53c
ReetamBG <r33t4m@gmail.com>
2024-12-28 15:52:47 +0530

    switched back db

--- database.py
+++ database.py
@@ -20,7 +20,7 @@ class User(Base):
 
 
 class DBHelper:
-	DATABASE_URL = "postgresql://postgres:6l1tch N1nj4@db.hondyywerwiuintsgjkw.supabase.co:5432/postgres"
+	DATABASE_URL = "postgresql://ree:fWC9JkeNvMtFA26nmXX04TaBoTQzQHr2@dpg-ctnb3htumphs73c4qbeg-a.singapore-postgres.render.com/trekathon"
 
 	def __init__(self):
 		self.engine = create_engine(self.DATABASE_URL)
